f94f7e410e2ade39c9761e0232a7ade36351f69b
soteloalarco <37395937+soteloalarco@users.noreply.github.com>
2020-05-11 22:00:52 -0500

    contenido agregado y parte B del algoritmo

--- CMMPP_algoritmo.py
+++ CMMPP_algoritmo.py
@@ -1,17 +1,19 @@
 import numpy as np  # NumPy package for arrays, random number generation, etc
 
 from clases.DeviceMTC import DeviceMTC
-from funciones.creardispositivos import creardispositivos
+from funciones.funcionesdispositivos import creardispositivos
 
 #Variables a modificar
 tiempoLimite = 0.02 # segundos, tiempo de paro del algoritmo
 deltaTiempo = 0.01 #segundos , diferencial de tiempo entre iteración
 numerosDecimalesDeltaTiempo=2 #Si se modifica deltaTiempo modificar también esta veriable
 
-dipositivos_Tipo1 = 1 # número de dispositivos de tipo 1
+dipositivos_Tipo1 = 3 # número de dispositivos de tipo 1, Control de iluminación
 lambdaRegular_Tipo1=1/60 # la tasa lambda para el estado regular de los dispositivos de tipo 1 (1 paquete cada 60 seg)
-dipositivos_Tipo2 = 2 # número de dispositivos de tipo 2
+dipositivos_Tipo2 = 2 # número de dispositivos de tipo 2, Monitoreo de consumo del agua y electricidad
 lambdaRegular_Tipo2=1/120 # la tasa lambda para el estado regular de los dispositivos de tipo 2 (0.5 paquete cada 60 seg)
+dipositivos_Tipo3 = 1 # número de dispositivos de tipo 3, Detección de terremotos
+lambdaRegular_Tipo3=1/180 # la tasa lambda para el estado regular de los dispositivos de tipo 2 (0.5 paquete cada 60 seg)
 
 #Inicialización de parámetros y variables
 n_Tipo1 = 0 # dispositivo de tipo1 inicial
@@ -22,16 +24,21 @@ dispositivos= [] # una lista para guardar las instancias de dipoitivos de distin
 #Se generan las instancias de cada tipo de dipositivos
 dispositivos.append(creardispositivos(dipositivos_Tipo1, lambdaRegular_Tipo1,'Control de iluminación'))
 dispositivos.append(creardispositivos(dipositivos_Tipo2, lambdaRegular_Tipo2,'Monitoreo de consumo del agua y electricidad'))
+dispositivos.append(creardispositivos(dipositivos_Tipo3, lambdaRegular_Tipo3,'Detección de terremotos'))
 
 ##########  Algoritmo CMMPP  #################
 
 for k in range(0,int(iteraciones + 1)): # Ciclo que avanza el tiempo
-
     print(tiempo)
+
+    theta = np.random.beta(3, 4, 1) # variable aleatoria beta para determinar Theta_n[k] = theta[k] * delta_n
 
     for dispositivosaux in dispositivos: # Ciclo que recorre todos los dispositivos
         for dispositivo in dispositivosaux:
             print(dispositivo.tipo)
+            Pnk= dispositivo.calcular_Pnk(theta)  # parte A del diagrama  /assets/CMMPP_diagrama.jpg
+            print(Pnk)
+            dispositivo.actualizarestado(Pnk)
 
 
     tiempo = round(tiempo + deltaTiempo, numerosDecimalesDeltaTiempo) # Función para redondear decimales
--- clases/DeviceMTC.py
+++ clases/DeviceMTC.py
@@ -15,4 +15,18 @@ class DeviceMTC(object):
     m_Pc = np.array(matriz_Pc)
     matriz_Pnk = []  # matriz de probabilidad de transición entre estados Pn[k]= Theta_n[k]*Pc + (1-Theta_n[k]*Pu)
 
+    def calcular_Pnk(self, theta):
+        thetank=theta*self.calcular_delta_n() # thetank= theta[k] * delta_n = Theta_n[k]
+        return (1 - thetank) * self.m_Pu + thetank * self.m_Pc #Pn[k]= Theta_n[k]*Pc + (1-Theta_n[k]*Pu)
+
+    def actualizarestado(self, pnk):
+        auxUniforme = np.random.uniform(0, 1, 1)
+        if self.estado==1 and auxUniforme > self.pnk[1][1]: # Si se está en estado alarma y si la variable uniforme es mayor a la probabilidad de que no cambie de estado, cambia de estado
+            self.estado=0
+        if self.estado==0 and auxUniforme > self.pnk[0][0]: # Si se está en estado normal y si la variable uniforme es mayor a la probabilidad de que no cambie de estado, cambia de estado
+            self.estado=1
+
+    def calcular_delta_n(self): #TODO: checar la distribución para el cálculo de delta_n
+        return np.random.normal(0.5,0.16,1) #variable aleatoria normal para delta_n
+
     #TODO: función para registrar petición en una lista y un archivo de texto
