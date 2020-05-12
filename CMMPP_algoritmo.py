2c383ef1723ba190e91e0b46c8b18bd838b1600f
soteloalarco <37395937+soteloalarco@users.noreply.github.com>
2020-05-12 13:47:58 -0500

    algoritmo terminado, restan algunos TODO's

--- CMMPP_algoritmo.py
+++ CMMPP_algoritmo.py
@@ -4,7 +4,7 @@ from clases.DeviceMTC import DeviceMTC
 from funciones.funcionesdispositivos import creardispositivos
 
 #Variables a modificar
-tiempoLimite = 0.02 # segundos, tiempo de paro del algoritmo
+tiempoLimite = 1 # segundos, tiempo de paro del algoritmo
 deltaTiempo = 0.01 #segundos , diferencial de tiempo entre iteración
 numerosDecimalesDeltaTiempo=2 #Si se modifica deltaTiempo modificar también esta veriable
 
@@ -22,23 +22,28 @@ iteraciones=tiempoLimite/deltaTiempo # las iteraciones  que se producirán recor
 dispositivos= [] # una lista para guardar las instancias de dipoitivos de distintos tipos
 
 #Se generan las instancias de cada tipo de dipositivos
-dispositivos.append(creardispositivos(dipositivos_Tipo1, lambdaRegular_Tipo1,'Control de iluminación'))
-dispositivos.append(creardispositivos(dipositivos_Tipo2, lambdaRegular_Tipo2,'Monitoreo de consumo del agua y electricidad'))
-dispositivos.append(creardispositivos(dipositivos_Tipo3, lambdaRegular_Tipo3,'Detección de terremotos'))
+dispositivos.append(creardispositivos(dipositivos_Tipo1, lambdaRegular_Tipo1,'Control de iluminación',tiempo))
+dispositivos.append(creardispositivos(dipositivos_Tipo2, lambdaRegular_Tipo2,'Monitoreo de consumo del agua y electricidad',tiempo))
+dispositivos.append(creardispositivos(dipositivos_Tipo3, lambdaRegular_Tipo3,'Detección de terremotos',tiempo))
 
 ##########  Algoritmo CMMPP  #################
 
 for k in range(0,int(iteraciones + 1)): # Ciclo que avanza el tiempo
     print(tiempo)
 
-    theta = np.random.beta(3, 4, 1) # variable aleatoria beta para determinar Theta_n[k] = theta[k] * delta_n
-
     for dispositivosaux in dispositivos: # Ciclo que recorre todos los dispositivos
+        theta = np.random.beta(3, 4, 1)  # variable aleatoria beta para determinar Theta_n[k] = theta[k] * delta_n , una distinta por cada aplicacion
         for dispositivo in dispositivosaux:
             print(dispositivo.tipo)
             Pnk= dispositivo.calcular_Pnk(theta)  # parte A del diagrama  /assets/CMMPP_diagrama.jpg
             print(Pnk)
-            dispositivo.actualizarestado(Pnk)
+            dispositivo.actualizarestado(Pnk) # parte B del diagrama
+            dispositivo.generararribo(tiempo) # parte C del diagrama
+
 
 
     tiempo = round(tiempo + deltaTiempo, numerosDecimalesDeltaTiempo) # Función para redondear decimales
+
+for arribo in dispositivo.registroArribos:
+    estadoAux= "normal" if arribo[3]==0 else "alarma"
+    print ("Instante: " + str(arribo[0]) +"     Identificador: " +str(arribo[2])+ ":"+str(arribo[1])+"      Estado: "+ estadoAux)
--- clases/DeviceMTC.py
+++ clases/DeviceMTC.py
@@ -3,30 +3,56 @@ import numpy as np  # NumPy package for arrays, random number generation, etc
 
 class DeviceMTC(object):
     # Definición de constructor
-    def __init__(self, lambdareg, Xpos, Ypos, estado,tipo):
+    def __init__(self, lambdareg, Xpos, Ypos, estado, tipo,tiempoInicial,identificador):
         self.lambdareg = lambdareg
-        self.posicion = [Xpos, Ypos] # la posición espacial dentro de la celula
-        self.estado = estado # estado regular o de alarma (0 es regular y 1 alarma)
-        self.tipo=tipo # tipo de dispositivo
+        self.posicion = [Xpos, Ypos]  # la posición espacial dentro de la celula
+        self.estado = estado  # estado regular o de alarma (0 es regular y 1 alarma)
+        self.tipo = tipo  # tipo de dispositivo
+        self.tiempoArribo = tiempoInicial # siguiente instante en el que se realizará una petición, debe iniciarse con el tiempo inicial
+        self.identificador = identificador
 
     matriz_Pu = [[1, 1], [0, 0]]  # matriz que describe el comportamiento no unsincronized
     m_Pu = np.array(matriz_Pu)
     matriz_Pc = [[0, 1], [1, 0]]  # matriz que describe el comportamiento sincronized
     m_Pc = np.array(matriz_Pc)
-    matriz_Pnk = []  # matriz de probabilidad de transición entre estados Pn[k]= Theta_n[k]*Pc + (1-Theta_n[k]*Pu)
+    #matriz_Pnk = []  # matriz de probabilidad de transición entre estados Pn[k]= Theta_n[k]*Pc + (1-Theta_n[k]*Pu)
+    registroArribos = []  # El conglomerado de arribos del estado normal y del de alarma
+    cuentaAlarmas = 0  # Contador que registra las veces que se estuvo en estado de alarma
 
     def calcular_Pnk(self, theta):
-        thetank=theta*self.calcular_delta_n() # thetank= theta[k] * delta_n = Theta_n[k]
-        return (1 - thetank) * self.m_Pu + thetank * self.m_Pc #Pn[k]= Theta_n[k]*Pc + (1-Theta_n[k]*Pu)
+        thetank = theta * self.calcular_delta_n()  # thetank= theta[k] * delta_n = Theta_n[k]
+        return (1 - thetank) * self.m_Pu + thetank * self.m_Pc  # Pn[k]= Theta_n[k]*Pc + (1-Theta_n[k]*Pu)
 
     def actualizarestado(self, pnk):
         auxUniforme = np.random.uniform(0, 1, 1)
-        if self.estado==1 and auxUniforme > self.pnk[1][1]: # Si se está en estado alarma y si la variable uniforme es mayor a la probabilidad de que no cambie de estado, cambia de estado
-            self.estado=0
-        if self.estado==0 and auxUniforme > self.pnk[0][0]: # Si se está en estado normal y si la variable uniforme es mayor a la probabilidad de que no cambie de estado, cambia de estado
-            self.estado=1
+        if self.estado == 1 and auxUniforme > pnk[1][1]:  # Si se está en estado alarma y si la variable uniforme es mayor a la probabilidad de que no cambie de estado, cambia de estado
+            self.estado = 0
+        if self.estado == 0 and auxUniforme > pnk[0][0]:  # Si se está en estado normal y si la variable uniforme es mayor a la probabilidad de que no cambie de estado, cambia de estado
+            self.estado = 1
 
-    def calcular_delta_n(self): #TODO: checar la distribución para el cálculo de delta_n
-        return np.random.normal(0.5,0.16,1) #variable aleatoria normal para delta_n
+    def generararribo(self, tiempo):
+        if self.estado == 1:  # alarma
+            self.generararriboalarma(tiempo)  # Generar exactamente 1 paquete
+            # TODO self.generarpaquetealarma , tamaño fijo o pareto? parte D del diagrama  /assets/CMMPP_diagrama.jpg
+        elif self.tiempoArribo <= tiempo:
+            self.generararribonormal()  # Generar un paquete en caso de que no exista ya uno
+            # TODO self.generarpaquetenormal , variable de pareto parte D del diagrama  /assets/CMMPP_diagrama.jpg
 
-    #TODO: función para registrar petición en una lista y un archivo de texto
+    def generararriboalarma(self, tiempo):
+        self.registroArribos.append([tiempo,self.tipo,self.identificador,self.estado])  # se registra el arribo en la lista
+        self.cuentaAlarmas = self.cuentaAlarmas + 1
+
+    def generararribonormal(self):
+        tiempoEspera = np.random.exponential(1 / (self.lambdareg),
+                                             1)  # el siguiente arribo se producirá segun una varible exponencial
+        self.tiempoArribo = self.tiempoArribo + tiempoEspera
+        self.registroArribos.append([int(self.tiempoArribo), self.tipo,self.identificador,self.estado])  # se registra el arribo en la lista
+
+    def calcular_delta_n(self):  # TODO: checar la distribución para el cálculo de delta_n
+        return np.random.normal(0.5, 0.16, 1)  # variable aleatoria normal para delta_n
+
+    # def generarpaquetenormal
+
+    # def generarpaquetealarma
+
+    # TODO: función para registrar petición en una lista y un archivo de texto, junto con el tamaño del paquete y otra información del dispositivo
